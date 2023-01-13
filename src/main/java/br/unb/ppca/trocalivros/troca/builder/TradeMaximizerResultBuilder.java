16114944b34a9f89593ffe44c2ae88bcf315be02
Demétrius de Almeida Jubé <jube@senado.leg.br>
2023-01-13 04:37:27 -0300

    Ajuste no log

--- src/main/java/br/unb/ppca/trocalivros/troca/builder/TradeMaximizerResultBuilder.java
+++ src/main/java/br/unb/ppca/trocalivros/troca/builder/TradeMaximizerResultBuilder.java
@@ -6,16 +6,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import lombok.extern.slf4j.Slf4j;
 import tm.TradeMaximizer;
 
-@Slf4j
 @Component
 public class TradeMaximizerResultBuilder {
+	
+	private final Logger log = LoggerFactory.getLogger(TradeMaximizerResultBuilder.class);
 
 	@Autowired
 	private TradeMaximizerInputBuilder tradeMaximizerInputBuilder;
